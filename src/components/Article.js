be3313a48d4f7a2ceae401dd7956b74bcfa0075c
BenjaminDexant <benjamin.dexant@gmail.com>
2020-07-23 23:45:23 +0200

    create cards article with material ui

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,58 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-function Article(article) {
+const useStyles = makeStyles((theme) => ({
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+function Article(article, key) {
+  const classes = useStyles();
   return (
-    <li>
-      <h1>{article.article.title}</h1>
-      <p>{article.article.date}</p>
-      <p>{article.article.content}</p>
-    </li>
+    <Grid item key={key} xs={12} sm={6} md={4}>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.cardMedia}
+          image={article.article.image_url_1}
+          title={article.article.title}
+        />
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {article.article.title}
+          </Typography>
+          <Typography>{article.article.date}</Typography>
+          <Typography>{article.article.content}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            View
+          </Button>
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
 export default Article;
