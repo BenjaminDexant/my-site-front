f22c30a039fc976851302b1495b7bc217583eb95
BenjaminDexant <benjamin.dexant@gmail.com>
2020-07-24 00:47:06 +0200

    fix bug navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Button, IconButton } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
 import HomeIcon from "@material-ui/icons/Home";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import logo from "../image/logo.jpg";
+import { useHistory } from "react-router-dom";
+
 import "./header.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -24,6 +25,14 @@ const useStyles = makeStyles((theme) => ({
 
 function Header() {
   const classes = useStyles();
+
+  const history = useHistory();
+  const goHome = () => {
+    return history.push(`/`);
+  };
+  const goLogin = () => {
+    return history.push(`/login`);
+  };
 
   return (
     <div className={classes.heroContent}>
@@ -44,11 +53,13 @@ function Header() {
         <div className={classes.heroButtons}>
           <Grid container spacing={1} justify="center">
             <Grid item>
-              <Link to="/">
-                <Button variant="outlined" color="primary">
-                  <HomeIcon color="inherit" /> Home
-                </Button>
-              </Link>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => goHome()}
+              >
+                <HomeIcon color="inherit" /> Home
+              </Button>
             </Grid>
             <Grid item>
               <Button variant="outlined" color="primary">
@@ -65,11 +76,13 @@ function Header() {
               </Button>
             </Grid>
             <Grid item>
-              <Link to="/login">
-                <Button variant="outlined" color="primary">
-                  <AddCircleOutlineIcon color="inherit" /> Login
-                </Button>
-              </Link>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => goLogin()}
+              >
+                <AddCircleOutlineIcon color="inherit" /> Login
+              </Button>
             </Grid>
           </Grid>
         </div>
