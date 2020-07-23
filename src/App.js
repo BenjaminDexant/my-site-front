4825f6f38281ec85eb4c2e0ddda1add7ed877059
BenjaminDexant <benjamin.dexant@gmail.com>
2020-07-23 13:26:34 +0200

    npm install router and add router in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import Login from "./components/Login";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import Article from "./components/Article";
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav />
-      <Login />
-      <MainPage />
+      <Switch>
+        <Route path="/login" exact component={Login} />
+      </Switch>
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/articles/:id_article" exact component={Article} />
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 export default App;
