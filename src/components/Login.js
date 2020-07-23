7a93bfeeceb327003b98db348386540a21f1fc6e
BenjaminDexant <benjamin.dexant@gmail.com>
2020-07-23 16:29:15 +0200

    npm install material ui and swalfire and create form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import { useHistory } from 'react-router-dom';
 
-function Login() {
+export default function Login() {
   return (
-    <>
-      <h1>Log me in</h1>
-    </>
+    <div>
+      <Container component="main" maxWidth="xs" className="connexion-form">
+        <CssBaseline />
+        <div>
+          <form>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              type="text"
+              id="name"
+              name="name"
+              label="name"
+              autoComplete="name"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              type="password"
+              name="password"
+              id="password"
+              label="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Log me in
+            </Button>
+          </form>
+        </div>
+      </Container>
+    </div>
   );
 }
-export default Login;
